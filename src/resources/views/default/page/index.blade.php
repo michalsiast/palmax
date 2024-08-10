a81ed4ef85da4c9021b77befb802ad143630abdc
Michał Siast <michalsiast@gmail.com>
2024-08-10 14:55:13 +0200

    Update index.blade.php

--- src/resources/views/default/page/index.blade.php
+++ src/resources/views/default/page/index.blade.php
@@ -75,7 +75,7 @@
                             <!-- About Image Wrap Start -->
                             <div class="about-img-wrap">
                                 <div class="about-img about-img-big">
-                                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/LYtYO2ibab4?si=yY3hnFGSJdgMs5DK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    <iframe width="100%" style="border-radius: 20px" height="350" src="https://www.youtube.com/embed/LYtYO2ibab4?si=yY3hnFGSJdgMs5DK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                                 </div>
                             </div>
                             <!-- About Image Wrap End -->
